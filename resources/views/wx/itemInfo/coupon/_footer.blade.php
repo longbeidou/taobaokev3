199bc9d5f05cec4da253da1778dd50d371cf4e26
wanglongshuai <1482585838@qq.com>
2018-06-25 09:28:50 +0800

    增加商品详情页底部导航的分享和领券的样式

--- resources/views/wx/itemInfo/coupon/_footer.blade.php
+++ resources/views/wx/itemInfo/coupon/_footer.blade.php
@@ -1,9 +1,18 @@
 <nav class="mui-bar mui-bar-tab lbd-footer-tab lbd-goods-footer" id="lbd-footer-tab-item">
-    <a class="mui-tab-item lbd-1" href="{{ route('wx.index') }}">
+    <a class="mui-tab-item" href="{{ route('wx.index') }}" style="width: 100%;">
         <span class="mui-icon mui-icon-home"></span>
         <span class="mui-tab-label">首页</span>
     </a>
-    <a class="mui-tab-item lbd-take" href="{{ route('wx.search.all') }}?q={{ $itemInfo->title }}">
+    <a class="mui-tab-item" href="https://www.baidu.com" style="width: 100%;">
+        <span class="mui-icon mui-icon-pengyouquan"></span>
+        <span class="mui-tab-label">分享</span>
+    </a>
+    <a class="mui-tab-item" href="{{ route('wx.search.all') }}?q={{ $itemInfo->title }}" style="width: 100%;">
+        <span class="mui-icon mui-icon-search"></span>
+        <span class="mui-tab-label">搜同类</span>
+    </a>
+
+    <a class="mui-tab-item lbd-take" style="width: 140px;">
         <span class="mui-tab-label">
           领取{{ floor($couponInfo->coupon_amount) }}元券
           <div class="lbd-left-circle lbd-01"></div>
