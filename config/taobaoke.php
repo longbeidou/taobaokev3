1b49dca8a0368b55f3479fa35e238741152d2924
wanglongshuai <1482585838@qq.com>
2018-06-27 21:46:50 +0800

    增加开启高级权限的配置文件

--- config/taobaoke.php
+++ config/taobaoke.php
@@ -14,5 +14,11 @@ return [
 		'logo' => '', // 如果启用，则此项必填
 		'text' => '', // 如果启用，则此项必填
 		'user_id' => ''
+	],
+	'advanced_permissions' => [ // 是否开启高级权限
+		'taobao_tbk_coupon_convert' => true, // 	【导购】链接转换 true表示开启
+		'taobao_tbk_item_convert' => true, // 淘宝客商品链接转换 true表示开启
+		'taobao_tbk_shop_convert' => true, // 淘宝客店铺链接转换 true表示开启
+		'taobao_tbk_tpwd_convert' => true  // 淘口令转链 true表示开启
 	]
 ];
