91dd5807e658825de2c54ef5ede9c5550d282001
wanglongshuai <1482585838@qq.com>
2018-07-03 18:04:42 +0800

    增加百度推送的js代码

--- config/website.php
+++ config/website.php
@@ -37,5 +37,18 @@ return [
 	'analysis_js_wx' => '<script src="https://s19.cnzz.com/z_stat.php?id=1274082543&web_id=1274082543" language="JavaScript"></script>',
 
 	// 百度推送
-	'baidu_push_js' => "",
+	'baidu_push_js' => "<script>
+(function(){
+    var bp = document.createElement('script');
+    var curProtocol = window.location.protocol.split(':')[0];
+    if (curProtocol === 'https') {
+        bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
+    }
+    else {
+        bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+    }
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(bp, s);
+})();
+</script>",
 ];
