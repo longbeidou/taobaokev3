2928cbc1b25fd030a2b3e21822745caee5ed016e
wanglongshuai <1482585838@qq.com>
2018-07-04 18:13:50 +0800

    修改栏目商品导航的显示数量

--- resources/views/wx/goodsCategory/_goods_category_sub.blade.php
+++ resources/views/wx/goodsCategory/_goods_category_sub.blade.php
@@ -2,14 +2,16 @@
 <div class="mui-row lbd-cate-list">
   <ul>
     @foreach($subGoodsCategory as $category)
-    <li>
-      <a class="lbd-a-no-tap" title="{{ $category->name }}淘宝优惠券商品" href="{{ route('goodsCategorys.categoryTwo', ['id' => $category->id]) }}">
-        <div class="lbd-img">
-          <img src="{{ $category->image }}" alt="{{ $category->name }}优惠券图片" />
-        </div>
-        <div class="lbd-text">{{ $category->name }}</div>
-      </a>
-    </li>
+      @if($key <= 8)
+      <li>
+        <a class="lbd-a-no-tap" title="{{ $category->name }}淘宝优惠券商品" href="{{ route('goodsCategorys.categoryTwo', ['id' => $category->id]) }}">
+          <div class="lbd-img">
+            <img src="{{ $category->image }}" alt="{{ $category->name }}优惠券图片" />
+          </div>
+          <div class="lbd-text">{{ $category->name }}</div>
+        </a>
+      </li>
+      @endif
     @endforeach
   </ul>
 </div>
--- resources/views/wx/goodsCategory/_goods_category_sub_2.blade.php
+++ resources/views/wx/goodsCategory/_goods_category_sub_2.blade.php
@@ -1,15 +1,17 @@
 <!--商品分类 开始-->
 <div class="mui-row lbd-cate-list">
   <ul>
-    @foreach($subGoodsCategory as $category)
-    <li>
-      <a class="lbd-a-no-tap" title="{{ $category->name }}淘宝优惠券商品" href="{{ route('goodsCategorys.categorySon', ['id' => $category->id]) }}">
-        <div class="lbd-img">
-          <img src="{{ $category->image }}" alt="{{ $category->name }}优惠券图片" />
-        </div>
-        <div class="lbd-text">{{ $category->name }}</div>
-      </a>
-    </li>
+    @foreach($subGoodsCategory as $key => $category)
+      @if($key <= 8)
+      <li>
+        <a class="lbd-a-no-tap" title="{{ $category->name }}淘宝优惠券商品" href="{{ route('goodsCategorys.categorySon', ['id' => $category->id]) }}">
+          <div class="lbd-img">
+            <img src="{{ $category->image }}" alt="{{ $category->name }}优惠券图片" />
+          </div>
+          <div class="lbd-text">{{ $category->name }}</div>
+        </a>
+      </li>
+      @endif
     @endforeach
   </ul>
 </div>
