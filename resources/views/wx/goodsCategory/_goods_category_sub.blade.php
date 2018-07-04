c595b7f500bdef03afebfc774afa08a89a0faa59
wanglongshuai <1482585838@qq.com>
2018-07-04 18:15:32 +0800

    修改商品列表图片报错

--- resources/views/wx/goodsCategory/_goods_category_sub.blade.php
+++ resources/views/wx/goodsCategory/_goods_category_sub.blade.php
@@ -1,7 +1,7 @@
 <!--商品分类 开始-->
 <div class="mui-row lbd-cate-list">
   <ul>
-    @foreach($subGoodsCategory as $category)
+    @foreach($subGoodsCategory as $key => $category)
       @if($key <= 8)
       <li>
         <a class="lbd-a-no-tap" title="{{ $category->name }}淘宝优惠券商品" href="{{ route('goodsCategorys.categoryTwo', ['id' => $category->id]) }}">
