eecdcbca7f8ae25aae3477b1299193eca1f9ac3d
wanglongshuai <1482585838@qq.com>
2018-07-05 15:22:45 +0800

    修改优惠券的adzoneeid配置文件

--- config/adzoneID.php
+++ config/adzoneID.php
@@ -2,23 +2,23 @@
 
 return [
 	// 无线端聚划算搜素的pid
-	'wx_ju_search_pid' => 'mm_46591525_34308672_121822521',
+	'wx_ju_search_pid' => 'mm_46591525_34308672_1459170411',
 
 	// 无线端全部搜索的adzone_id
-	'wx_search_all_adzone_id' => '770398581',
+	'wx_search_all_adzone_id' => '1459124850',
 
 	// 无线端 只针对天猫的搜索
-	'wx_search_tmall_adzone_id' => '770398581',
+	'wx_search_tmall_adzone_id' => '1459162255',
 
 	// 淘口令 搜索
 	'wx_search_tpwd' => '770398581',
 
 	// 无线端首页coupon api获取的商品
-	'wx_index_coupon_adzone_id' => '770398581',
+	'wx_index_coupon_adzone_id' => '1459254918',
 
 	// 无线端猜你喜欢 coupon api获取的商品
-	'wx_coupon_guess_you_like' => '770398581',
+	'wx_coupon_guess_you_like' => '1459300118',
 
 	// 无线端淘抢购
-	'wx_tqg' => '770398581'
+	'wx_tqg' => '1459318297'
 ];
