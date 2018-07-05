a39dbef04d76ff8d1b8b1dbfa6ae3302b0254ff0
wanglongshuai <1482585838@qq.com>
2018-07-05 13:16:07 +0800

    增加淘宝客物料下行-导购api对应的物料配置文件

---
+++ config/category.php
@@ -0,0 +1,78 @@
+<?php
+
+return [
+	// 好券直播
+	0 => [
+		'name' => '好券直播',
+		'rules' => [
+			0 => ['category' => '综合', 'page_size' => '20', 'adzone_id' => '', 'material_id' => '3756'],
+			1 => ['category' => '女装', 'page_size' => '20', 'adzone_id' => '', 'material_id' => '3767'],
+			2 => ['category' => '家居家装', 'page_size' => '20', 'adzone_id' => '', 'material_id' => '3758'],
+			3 => ['category' => '数码家电', 'page_size' => '20', 'adzone_id' => '', 'material_id' => '3759'],
+			4 => ['category' => '鞋包配饰', 'page_size' => '20', 'adzone_id' => '', 'material_id' => '3762'],
+			5 => ['category' => '美妆个护', 'page_size' => '20', 'adzone_id' => '', 'material_id' => '3763'],
+			6 => ['category' => '男装', 'page_size' => '20', 'adzone_id' => '', 'material_id' => '3764'],
+			7 => ['category' => '内衣', 'page_size' => '20', 'adzone_id' => '', 'material_id' => '3765'],
+			8 => ['category' => '母婴', 'page_size' => '20', 'adzone_id' => '', 'material_id' => '3760'],
+			9 => ['category' => '食品', 'page_size' => '20', 'adzone_id' => '', 'material_id' => '3761'],
+			10 => ['category' => '运动户外', 'page_size' => '20', 'adzone_id' => '', 'material_id' => '3766']
+		]
+	],
+	// 品牌券
+	1 => [
+		'name' => '大牌券',
+		'rules' => [
+			0 => ['category' => '综合', 'page_size' => '20', 'adzone_id' => '', 'material_id' => '3786'],
+			1 => ['category' => '女装', 'page_size' => '20', 'adzone_id' => '', 'material_id' => '3788'],
+			2 => ['category' => '家居家装', 'page_size' => '20', 'adzone_id' => '', 'material_id' => '3792'],
+			3 => ['category' => '数码家电', 'page_size' => '20', 'adzone_id' => '', 'material_id' => '3793'],
+			4 => ['category' => '鞋包配饰', 'page_size' => '20', 'adzone_id' => '', 'material_id' => '3796'],
+			5 => ['category' => '美妆个护', 'page_size' => '20', 'adzone_id' => '', 'material_id' => '3794'],
+			6 => ['category' => '男装', 'page_size' => '20', 'adzone_id' => '', 'material_id' => '3790'],
+			7 => ['category' => '内衣', 'page_size' => '20', 'adzone_id' => '', 'material_id' => '3787'],
+			8 => ['category' => '母婴', 'page_size' => '20', 'adzone_id' => '', 'material_id' => '3789'],
+			9 => ['category' => '食品', 'page_size' => '20', 'adzone_id' => '', 'material_id' => '3791'],
+			10 => ['category' => '运动户外', 'page_size' => '20', 'adzone_id' => '', 'material_id' => '3795']
+		]
+	],
+	// 母婴专场
+	2 => [
+		'name' => '母婴专场',
+		'rules' => [
+			0 => ['category' => '备孕阶段', 'page_size' => '20', 'adzone_id' => '', 'material_id' => '4040'],
+			1 => ['category' => '0-6个月', 'page_size' => '20', 'adzone_id' => '', 'material_id' => '4041'],
+			2 => ['category' => '7-12个月', 'page_size' => '20', 'adzone_id' => '', 'material_id' => '4042'],
+			3 => ['category' => '1-3岁', 'page_size' => '20', 'adzone_id' => '', 'material_id' => '4043'],
+			4 => ['category' => '4-6岁', 'page_size' => '20', 'adzone_id' => '', 'material_id' => '4044'],
+			5 => ['category' => '7-12岁', 'page_size' => '20', 'adzone_id' => '', 'material_id' => '4045']
+		]
+	],
+	// 聚划算拼团
+	3 => [
+		'name' => '拼团专场',
+		'rules' => [
+			0 => ['category' => '拼团专场', 'page_size' => '20', 'adzone_id' => '', 'material_id' => '4071']
+		]
+	],
+	// 潮流范
+	4 => [
+		'name' => '潮流范',
+		'rules' => [
+			0 => ['category' => '潮流范', 'page_size' => '20', 'adzone_id' => '', 'material_id' => '4093']
+		]
+	],
+	// 有好货
+	5 => [
+		'name' => '有好货',
+		'rules' => [
+			0 => ['category' => '有好货', 'page_size' => '20', 'adzone_id' => '', 'material_id' => '4092']
+		]
+	],
+	// 特惠
+	6 => [
+		'name' => '特惠专场',
+		'rules' => [
+			0 => ['category' => '特惠专场', 'page_size' => '20', 'adzone_id' => '', 'material_id' => '4094']
+		]
+	]
+];
