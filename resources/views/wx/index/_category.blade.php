eb76a2d36e8d41ae7948fbad4aef222cccd1f5dd
wanglongshuai <1482585838@qq.com>
2018-07-10 12:01:20 +0800

    增加无线端首页栏目的图片

--- resources/views/wx/index/_category.blade.php
+++ resources/views/wx/index/_category.blade.php
@@ -3,31 +3,31 @@
   <ul>
     <li>
       <a class="a-can-do" href="{{ route('wx.optimusMaterial.pintuan') }}" title="聚划算拼团">
-        <div class="lbd-img"><img src="http://placehold.it/40x40"></div>
+        <div class="lbd-img"><img src="/wxstyle/images/categorys/pintuan.png"></div>
         <div class="lbd-text">拼团</div>
       </a>
     </li>
     <li>
       <a class="a-can-do" href="{{ route('wx.optimusMaterial.fashion') }}" title="淘宝天猫优惠券潮流范专场">
-        <div class="lbd-img"><img src="http://placehold.it/40x40"></div>
+        <div class="lbd-img"><img src="/wxstyle/images/categorys/fashion.png"></div>
         <div class="lbd-text">潮流街区</div>
       </a>
     </li>
     <li>
       <a class="a-can-do" href="{{ route('wx.optimusMaterial.recommend') }}" title="淘宝内部优惠券推荐专场">
-        <div class="lbd-img"><img src="http://placehold.it/40x40"></div>
+        <div class="lbd-img"><img src="/wxstyle/images/categorys/recommend.png"></div>
         <div class="lbd-text">推荐好货</div>
       </a>
     </li>
     <li>
       <a class="a-can-do" href="{{ route('wx.optimusMaterial.sales') }}" title="特价淘宝天猫优惠券专场">
-        <div class="lbd-img"><img src="http://placehold.it/40x40"></div>
+        <div class="lbd-img"><img src="/wxstyle/images/categorys/sales.png"></div>
         <div class="lbd-text">特惠专场</div>
       </a>
     </li>
     <li>
       <a class="a-can-do" href="{{ route('wx.taoqianggou.index') }}" title="淘抢购">
-        <div class="lbd-img"><img src="http://placehold.it/40x40"></div>
+        <div class="lbd-img"><img src="/wxstyle/images/categorys/qiang.png"></div>
         <div class="lbd-text">整点秒杀</div>
       </a>
     </li>
