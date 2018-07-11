1198e633479a549d308eabb1a439028a83005969
wanglongshuai <1482585838@qq.com>
2018-07-11 23:18:19 +0800

    修改路由，只让PC端可以访问管理员后台的相关路由

--- routes/web.php
+++ routes/web.php
@@ -4,20 +4,22 @@
 Auth::routes();
 Route::get('/home', 'Home\HomeController@index')->name('home');
 
-// 管理员后台登录
-Route::prefix('admin')->group(function() {
-	Route::get('login', 'Admin\LoginController@login')->name('admin.login');
-  Route::post('login', 'Admin\LoginController@doLogin')->name('admin.dologin');
-  Route::get('logout', 'Admin\LoginController@logout')->name('admin.logout');
-  Route::get('dashboard', 'Admin\DashboardController@dashboard')->name('admin.dashboard');
-  Route::get('changepassword', 'Admin\UserController@changePassword')->name('admin.changePassword');
-  Route::post('changepassword', 'Admin\UserController@doChangePassword')->name('admin.doChangePassword');
-  Route::resource('goodsCategorys', 'Admin\GoodsCategoryController');
-  Route::get('couponRules/{id}', 'Admin\CouponRulesController@show')->name('admin.couponRule.show');
-  Route::get('couponRules/{id}/edit', 'Admin\CouponRulesController@edit')->name('admin.couponRule.edit');
-  Route::post('couponRules/', 'Admin\CouponRulesController@store')->name('admin.couponRule.store');
-  Route::get('couponRules/{id}/create', 'Admin\CouponRulesController@create')->name('admin.couponRule.create');
-  Route::post('couponRules/{id}/update', 'Admin\CouponRulesController@update')->name('admin.couponRule.update');
+Route::domain(env('DOMAIN_PC'))->group(function() {
+	// 管理员后台登录
+	Route::prefix('admin')->group(function() {
+		Route::get('login', 'Admin\LoginController@login')->name('admin.login');
+		Route::post('login', 'Admin\LoginController@doLogin')->name('admin.dologin');
+		Route::get('logout', 'Admin\LoginController@logout')->name('admin.logout');
+		Route::get('dashboard', 'Admin\DashboardController@dashboard')->name('admin.dashboard');
+		Route::get('changepassword', 'Admin\UserController@changePassword')->name('admin.changePassword');
+		Route::post('changepassword', 'Admin\UserController@doChangePassword')->name('admin.doChangePassword');
+		Route::resource('goodsCategorys', 'Admin\GoodsCategoryController');
+		Route::get('couponRules/{id}', 'Admin\CouponRulesController@show')->name('admin.couponRule.show');
+		Route::get('couponRules/{id}/edit', 'Admin\CouponRulesController@edit')->name('admin.couponRule.edit');
+		Route::post('couponRules/', 'Admin\CouponRulesController@store')->name('admin.couponRule.store');
+		Route::get('couponRules/{id}/create', 'Admin\CouponRulesController@create')->name('admin.couponRule.create');
+		Route::post('couponRules/{id}/update', 'Admin\CouponRulesController@update')->name('admin.couponRule.update');
+	});
 });
 
 Route::domain(env('DOMAIN_WX'))->group(function() {
