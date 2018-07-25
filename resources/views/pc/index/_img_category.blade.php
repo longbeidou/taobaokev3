3f9f90306c5add6f0805695b1956e693794c30b7
wanglongshuai <1482585838@qq.com>
2018-07-25 13:10:41 +0800

    调整首页图片分类的图片的默认大小

--- resources/views/pc/index/_img_category.blade.php
+++ resources/views/pc/index/_img_category.blade.php
@@ -3,10 +3,10 @@
         <div class="row">
             <div class="col-xs-12">
                 <ul class="list-inline">
-                    <li><a href="" title="" target="_blank"><img src="http://placehold.it/100x50"></a></li>
-                    <li><a href="" title="" target="_blank"><img src="http://placehold.it/100x50"></a></li>
-                    <li><a href="" title="" target="_blank"><img src="http://placehold.it/100x50"></a></li>
-                    <li><a href="" title="" target="_blank"><img src="http://placehold.it/100x50"></a></li>
+                    <li><a href="" title="" target="_blank"><img src="http://placehold.it/300x150"></a></li>
+                    <li><a href="" title="" target="_blank"><img src="http://placehold.it/300x150"></a></li>
+                    <li><a href="" title="" target="_blank"><img src="http://placehold.it/300x150"></a></li>
+                    <li><a href="" title="" target="_blank"><img src="http://placehold.it/300x150"></a></li>
                 </ul>
             </div>
         </div>
