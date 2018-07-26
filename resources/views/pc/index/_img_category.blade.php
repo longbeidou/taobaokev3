7234ce25ef4d8065542947df9395ac67d81254cd
wanglongshuai <1482585838@qq.com>
2018-07-26 18:21:06 +0800

    修改PC端首页的分类图片

--- resources/views/pc/index/_img_category.blade.php
+++ resources/views/pc/index/_img_category.blade.php
@@ -5,8 +5,8 @@
                 <ul class="list-inline">
                     <li><a href="{{ route('pc.optimusMaterial.pintuan') }}" title="聚划算拼团专场" target="_blank"><img src="/pcstyle/images/category_pintuan.jpg"  alt="聚划算拼团专场"></a></li>
                     <li><a href="{{ route('pc.optimusMaterial.fashion') }}" title="淘宝天猫优惠券潮流范专场" target="_blank"><img src="/pcstyle/images/category_fashion.jpg"  alt="淘宝天猫优惠券潮流范专场"></a></li>
-                    <li><a href="{{ route('pc.optimusMaterial.sales') }}" title="特价淘宝天猫优惠券专场" target="_blank"><img src="/pcstyle/images/category_sales.jpg"  alt="特价淘宝天猫优惠券专场"></a></li>
                     <li><a href="{{ route('pc.optimusMaterial.recommend') }}" title="淘宝内部优惠券推荐专场" target="_blank"><img src="/pcstyle/images/category_recommend.jpg"  alt="淘宝内部优惠券推荐专场"></a></li>
+                    <li><a href="{{ route('pc.optimusMaterial.sales') }}" title="特价淘宝天猫优惠券专场" target="_blank"><img src="/pcstyle/images/category_sales.jpg"  alt="特价淘宝天猫优惠券专场"></a></li>
                 </ul>
             </div>
         </div>
