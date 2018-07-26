8849689b2722815e38c0366e7f3084cc9feda9ce
wanglongshuai <1482585838@qq.com>
2018-07-26 16:50:41 +0800

    想淘抢购的图片

--- resources/views/pc/taoQiangGou/_banner.blade.php
+++ resources/views/pc/taoQiangGou/_banner.blade.php
@@ -2,7 +2,7 @@
     <div class="container box">
         <div class="row">
             <div class="col-xs-12 text-center">
-              <img src="/pcstyle/images/tqg.jpg" alt="">
+              <img src="/pcstyle/images/nav_tqg.jpg" alt="">
             </div>
         </div>
     </div>
